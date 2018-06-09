05a30c70b635ca95085f34ec483d0cf279c11630
Kombiah Ramaiah <35794629+kombiahrk@users.noreply.github.com>
2018-06-10 01:32:38 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,14 +3,14 @@
 	function generateChecksum($transId, $sellingCurrencyAmount, $accountingCurrencyAmount, $status, $rkey, $key)
 	{	
 		$str = "$transId|$sellingCurrencyAmount|$accountingCurrencyAmount|$status|$rkey|$key";
-        $generatedCheckSum = md5($str);
+        	$generatedCheckSum = md5($str);
 		return $generatedCheckSum;
 	}
 
 	function verifyChecksum($paymentTypeId, $transId, $userId, $userType, $transactionType, $invoiceIds, $debitNoteIds, $description, $sellingCurrencyAmount, $accountingCurrencyAmount, $key, $checksum)
 	{
 		$str = "$paymentTypeId|$transId|$userId|$userType|$transactionType|$invoiceIds|$debitNoteIds|$description|$sellingCurrencyAmount|$accountingCurrencyAmount|$key";
-        $generatedCheckSum = md5($str);
+        	$generatedCheckSum = md5($str);
         
 		if($generatedCheckSum == $checksum)
 			return true ;
